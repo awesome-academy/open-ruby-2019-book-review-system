bddd1c1194185dbf35c69a63e4316aab3c257c81
leduccuong1612 <leduccuong1612@gmail.com>
2020-03-03 10:33:48 +0700

    init Layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,7 @@
 Rails.application.routes.draw do
+  scope "(:locale)", locale: /#{I18n.available_locales.join('|')}/ do
+    root "static_pages#home"
+    get "/login", to: "sessions#new"
+    get "/signup", to: "users#new"
+  end
 end
