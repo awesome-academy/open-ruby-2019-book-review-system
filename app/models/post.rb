cda75b6d536697f24d90b14ba5efc8fec3fbe4a7
leduccuong1612 <leduccuong1612@gmail.com>
2020-03-12 16:02:09 +0700

    Search User

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,8 +5,10 @@ class Post < ApplicationRecord
   has_many :pictures, dependent: :destroy
 
   delegate :name, to: :user, prefix: :user
+  delegate :name, to: :book, prefix: :book
 
   scope :order_desc, ->{order created_at: :desc}
+  scope :find_post, ->(book_ids){where book_id:  book_ids}
 
   enum status: {locked: 0, opened: 1}
 end
